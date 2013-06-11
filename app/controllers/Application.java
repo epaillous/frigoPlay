05d25f510de97cb4b28cd0aa91e35f38846ed13e
Maïlys Reneaume <mailys.reneaume@gmail.com>
2013-06-11 12:10:03 +0200

    re

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,21 +1,24 @@
 package controllers;
-import play.db.jpa.Model;
-import play.*;
-import play.mvc.*;
-import play.mvc.results.Result;
-
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.*;
 
+import models.EtatFrigo;
+import models.User;
 
-import models.*;
+import play.mvc.Before;
+import play.mvc.Controller;
+import play.mvc.With;
 
+@With(Secure.class)
 public class Application extends Controller {
+	
+	@Before
+    static void setConnectedUser() {
+        if(Security.isConnected()) {
+            User user = User.find("byEmail", Security.connected()).first();
+            renderArgs.put("user", user);
+        }
+    }
 
     public static void index() {
     	EtatFrigo etatFrigo = EtatFrigo.find("order by date desc").first();
@@ -49,39 +52,62 @@ public class Application extends Controller {
     	render(utilisateurs);
     }
     
-//    public static void upload() {
-//    	render();
-//    }
-//
-//    
-    public static void upload() {
-    	InputStream inputStream = request.body;
-
-    	// Save it somewhere..
-    	// String filePath = "C:\\Utilisateurs\\Lili\\image.jpg";
-    	try 
-    	{ 
-    	
-    		File fichier = new File("C:\\image2.jpg\\"); 
-    	
-    	fichier.setWritable(true);
-    	fichier.createNewFile(); // Cette fonction doit �tre appel�e au sein d'un bloc TRY 
-    	java.io.FileOutputStream monFluxFichier = new java.io.FileOutputStream(fichier); // Doit �tre utilis� dans un bloc TRY 
-    	
-
-			byte[] buf = new byte [1024] ;
-	    	int len;
-	    	while((len=inputStream.read(buf))>0) monFluxFichier.write(buf,0,len);
-	    	monFluxFichier.close();
-	    	inputStream.close();
-	    }catch (IOException e) 
-	    	{ 
-	    	System.out.println("Impossible de cr�er le fichier"); 
-	    	e.printStackTrace();
-	    	} 
-    	
-    	// Anything which is appensed to POST URL at client end maps to String name
-    	System.out.println("This Method Called");
+    public static void photo() {
     	render();
     }
+    
+    
+ // fonction qui crée objet Photo  
+//    public static void upload(Photo PhotoSauvee) {
+//    	// correct si paramètre de type Upload
+//    	// File to = Play.getFile("data/" + file.getFileName());
+//    	//PhotoSauvee.save();
+//    }   
+
+//    public static void affichePhoto(long id){
+//    	
+//    	final Photo photo = Photo.findById(id);
+//    	notFoundIfNull(photo);
+//    	response.setContentTypeIfNotSet(photo.photo.type());
+//    	renderBinary(photo.photo.get());
+//    }
+//    
+ 			// Fonction qui devrait marcher parce que ça fonctionne quand le post est fait à partir d'un formulaire
+//            public static void upload(File[] files) {
+//              System.out.println("sortie upload");
+//                if (files == null || files.length < 1){
+//                	System.out.println("files null");
+//                    return;
+//                }
+//                File file = files[0];
+//                File uploadDir = new File(Play.applicationPath, "/public/uploads/");
+//                if (!uploadDir.exists()){
+//                    uploadDir.mkdirs();
+//                }
+//                try {
+//                    FileUtils.moveFile(file, new File(uploadDir, file.getName()));
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//                render();
+//            }
+
+            // fonction qui fait apparaître le .jpg temporaire à cause du pointeur null 
+       public static void upload(File file) {
+    	   final Map<String, String[]> entries = params.data;
+    	   System.out.println("val entree : " + entries.toString());
+//    		//notFoundIfNull(file);
+//    		File to = Play.getFile("data/" + file.getName());
+//    		file.renameTo(to);
+//    		try {
+//    			Files.copy(file, to);
+//    		} catch (RuntimeException e){
+//    			Play.getFile("data").mkdir();
+//    			Files.copy(file, to);
+//    		   	e.printStackTrace();
+//    		}
+    System.out.println("sortie upload");
+    	
+    }
+    
 }
