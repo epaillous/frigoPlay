d5bfeddf18371c5f6bb3e6a86f6db7d83548955a
Emilie Paillous <emilie.paillous@gmail.com>
2013-06-11 16:09:29 +0200

    c ok

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,12 +1,19 @@
 package controllers;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.*;
+
+import org.apache.commons.io.FileUtils;
 
 import models.EtatFrigo;
 import models.User;
 
+import play.Play;
 import play.mvc.Before;
 import play.mvc.Controller;
+import play.mvc.Http;
 import play.mvc.With;
 
 
@@ -21,9 +28,10 @@ public class Application extends Controller {
         }
     }
 
-    public static void index() {
-        render();
-    }
+	
+	public static void index() {
+		render();
+	}
     
     public static void historique() {
     	render();
@@ -51,27 +59,35 @@ public class Application extends Controller {
     
     public static void photo() {
     	render();
-    }
-       
- 			// Fonction qui devrait marcher parce que ça fonctionne quand le post est fait à partir d'un formulaire
-//            public static void upload(File[] files) {
-//              System.out.println("sortie upload");
-//                if (files == null || files.length < 1){
-//                	System.out.println("files null");
-//                    return;
-//                }
-//                File file = files[0];
-//                File uploadDir = new File(Play.applicationPath, "/public/uploads/");
-//                if (!uploadDir.exists()){
-//                    uploadDir.mkdirs();
-//                }
-//                try {
-//                    FileUtils.moveFile(file, new File(uploadDir, file.getName()));
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//                render();
-//            }
+    }      
 
+    
+ 			 //Fonction qui devrait marcher parce que ça fonctionne quand le post est fait à partir d'un formulaire
+    public static void upload(File[] files) {
+    	if (files == null || files.length < 1){
+    		System.out.println("files null");
+    		return;
+    	}
+    	File file = files[0];
+    	File uploadDir = new File(Play.applicationPath, "/public/uploads/");
+    	if (!uploadDir.exists()){
+    		uploadDir.mkdirs();
+    	}
+    	try {
+    		FileUtils.moveFile(file, new File(uploadDir, file.getName()));
+    	} catch (IOException e) {
+    		e.printStackTrace();
+    	}
+    }
+    
+//    
+//    public static void upload(){
+//    	FileInputStream stream = (FileInputStream) Http.Request.current().body;
+//    	
+//    	File uploadDir = new File(Play.applicationPath, "/public/uploads/");
+//    	if (!uploadDir.exists()){
+//   		uploadDir.mkdirs();
+//    	}
+//    }
     
 }
