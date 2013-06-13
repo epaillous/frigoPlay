ff82470d95d8d286f323f90a28abd762f9fa9989
Emilie Paillous <emilie.paillous@gmail.com>
2013-06-13 19:38:00 +0200

    suppression article ok

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -21,70 +21,53 @@ import play.mvc.Before;
 import play.mvc.Controller;
 import play.mvc.Http;
 import play.mvc.With;
+import utils.ApplicationUtils;
 
 
 @With(Secure.class)
 public class Application extends Controller {
-	
+
 	@Before
-    static void setConnectedUser() {
-        if(Security.isConnected()) {
-            User user = User.find("byEmail", Security.connected()).first();
-            renderArgs.put("user", user);
-        }
-    }
-	
+	static void setConnectedUser() {
+		if(Security.isConnected()) {
+			User user = User.find("byEmail", Security.connected()).first();
+			renderArgs.put("user", user);
+		}
+	}
+
 	@Before
-    static void listeCourante() {
-            User user = User.find("byEmail", Security.connected()).first();
-            ListeDeCourse listeCourante = user.listeDeCourse.get(0);
-            List<Aliment> articles = listeCourante.article;
-            List<Aliment> artFruitsLegumes = new ArrayList<Aliment>();
-    		List<Aliment> artViandes = new ArrayList<Aliment>();
-    		List<Aliment> artLaitages = new ArrayList<Aliment>();
-    		List<Aliment> artBoissons= new ArrayList<Aliment>() ;
-    		List<Aliment> artAutre = new ArrayList<Aliment>();
-    		List<Aliment> artEpicerie = new ArrayList<Aliment>();
-    		
-    		Iterator iter = articles.iterator();	
-    		while (iter.hasNext()) {
-    			Aliment cour = (Aliment) iter.next();
-    			switch (cour.section.name()) {
-    			case "FruitsLegumes":
-    				artFruitsLegumes.add(cour);
-    				break;
-    			case "Laitages":
-    				artLaitages.add(cour);
-    				break;	
-    			case "Viandes":
-    				artViandes.add(cour);
-    				break;	
-    			case "Boissons":
-    				artBoissons.add(cour);
-    				break;
-    			case "Autre":
-    				artAutre.add(cour);
-    				break;	
-    			case "Epicerie":
-    				artEpicerie.add(cour);
-    				break;
-    			
-    			default:
-    				break;
-    			}
-    		}	
-           
-            renderArgs.put("artFruitsLegumes", artFruitsLegumes);
-            renderArgs.put("artLaitages", artLaitages);
-            renderArgs.put("artViandes", artViandes);
-            renderArgs.put("artBoissons", artBoissons);
-            renderArgs.put("artAutre", artAutre);
-            renderArgs.put("artEpicerie", artEpicerie);
-    }
-	
-	public static void index() {
+	static void listeCourante() {
+		
+		/* On recupère l'utilisateur en session */
 		User user = User.find("byEmail", Security.connected()).first();
-		// Premier état frigo de la liste 
+
+		/* On recupère sa liste de course courante (non nulle) */
+		ListeDeCourse listeCourante = user.listeDeCourse.get(0);
+		List<Aliment> articles = listeCourante.article;
+		List<Aliment> artFruitsLegumes = new ArrayList<Aliment>();
+		List<Aliment> artViandes = new ArrayList<Aliment>();
+		List<Aliment> artLaitages = new ArrayList<Aliment>();
+		List<Aliment> artBoissons= new ArrayList<Aliment>() ;
+		List<Aliment> artAutre = new ArrayList<Aliment>();
+		List<Aliment> artEpicerie = new ArrayList<Aliment>();
+
+		/* tri par section */
+		ApplicationUtils.misAJourListes(articles, artFruitsLegumes, artViandes, artLaitages, artBoissons, artAutre, artEpicerie);
+
+		renderArgs.put("artFruitsLegumes", artFruitsLegumes);
+		renderArgs.put("artLaitages", artLaitages);
+		renderArgs.put("artViandes", artViandes);
+		renderArgs.put("artBoissons", artBoissons);
+		renderArgs.put("artAutre", artAutre);
+		renderArgs.put("artEpicerie", artEpicerie);
+	}
+
+	public static void index() {
+		
+		/* On recupère l'utilisateur en session */
+		User user = User.find("byEmail", Security.connected()).first();
+
+		/* On recupère le dernier état de son frigo */
 		EtatFrigo dernierEtat = EtatFrigo.find("user like ? order by date desc", user).first();
 		List<Aliment> aliments = dernierEtat.aliment;
 		List<Aliment> fruitsLegumes = new ArrayList<Aliment>();
@@ -93,40 +76,20 @@ public class Application extends Controller {
 		List<Aliment> boissons= new ArrayList<Aliment>() ;
 		List<Aliment> autre = new ArrayList<Aliment>();
 		List<Aliment> epicerie = new ArrayList<Aliment>();
-		
-		Iterator iter = aliments.iterator();	
-		while (iter.hasNext()) {
-			Aliment cour = (Aliment) iter.next();
-			switch (cour.section.name()) {
-			case "FruitsLegumes":
-				fruitsLegumes.add(cour);
-				break;
-			case "Laitages":
-				laitages.add(cour);
-				break;	
-			case "Viandes":
-				viandes.add(cour);
-				break;	
-			case "Boissons":
-				boissons.add(cour);
-				break;
-			case "Autre":
-				autre.add(cour);
-				break;	
-			case "Epicerie":
-				epicerie.add(cour);
-				break;
-			
-			default:
-				break;
-			}
-		}	
+
+		/* tri par section */
+		ApplicationUtils.misAJourListes(aliments, fruitsLegumes, viandes, laitages, boissons, autre, epicerie);
+
 		render(fruitsLegumes, viandes, laitages, boissons, autre, epicerie, dernierEtat);
 
 	}
-	
+
 	public static void ancienEtat(Long id) {
+		
+		/* On recupère l'état frigo sur lequel on a cliqué */
 		EtatFrigo etatFrigo = EtatFrigo.findById(id);
+
+		/* On recupère sa liste d'aliment */
 		List<Aliment> aliments = etatFrigo.aliment;
 		List<Aliment> fruitsLegumes = new ArrayList<Aliment>();
 		List<Aliment> viandes = new ArrayList<Aliment>();
@@ -134,113 +97,78 @@ public class Application extends Controller {
 		List<Aliment> boissons= new ArrayList<Aliment>() ;
 		List<Aliment> autre = new ArrayList<Aliment>();
 		List<Aliment> epicerie = new ArrayList<Aliment>();
-		
-		Iterator iter = aliments.iterator();	
-		while (iter.hasNext()) {
-			Aliment cour = (Aliment) iter.next();
-			switch (cour.section.name()) {
-			case "FruitsLegumes":
-				fruitsLegumes.add(cour);
-				break;
-			case "Laitages":
-				laitages.add(cour);
-				break;	
-			case "Viandes":
-				viandes.add(cour);
-				break;	
-			case "Boissons":
-				boissons.add(cour);
-				break;
-			case "Autre":
-				autre.add(cour);
-				break;	
-			case "Epicerie":
-				epicerie.add(cour);
-				break;
-			
-			default:
-				break;
-			}
-		}
+
+		/* tri par section */
+		ApplicationUtils.misAJourListes(aliments, fruitsLegumes, viandes, laitages, boissons, autre, epicerie);
+
 		render(fruitsLegumes, viandes, laitages, boissons, autre, epicerie, etatFrigo);
 	}
- 
-    public static void historique() {
-    	render();
-    }
-    
-    public static void recettesProposees() {
-    	render();
-    }
-    public static void recettesFavorites() {
-    	render();
-    }
-    public static void carottesRapees() {
-    	render();
-    }
-    
-    public static void listesCourses() {
-    	render();
-    }
-    
-    public static void archives() {
-    	render();
-    }
-    
-    public static void profil() {
-    	render();
-    }
-    
-    public static void photo() {
-    	render();
-    }   
-    
-    public static void ajoutAlimentListe(String aliment) {
-    	User user = User.find("byEmail", Security.connected()).first();
-    	ListeDeCourse listeCourante = user.listeDeCourse.get(0);
-    	listeCourante.addAliment(aliment);
-    	List<Aliment> articles = listeCourante.article;
-        List<Aliment> artFruitsLegumes = new ArrayList<Aliment>();
+
+	public static void historique() {
+		render();
+	}
+
+	public static void recettesProposees() {
+		render();
+	}
+	public static void recettesFavorites() {
+		render();
+	}
+	public static void carottesRapees() {
+		render();
+	}
+
+	public static void listesCourses() {
+		render();
+	}
+
+	public static void archives() {
+		render();
+	}
+
+	public static void profil() {
+		render();
+	}
+
+	public static void photo() {
+		render();
+	}   
+
+	public static void ajoutAlimentListe(String aliment) {
+		
+		/* On recupère l'utilisateur en session */
+		User user = User.find("byEmail", Security.connected()).first();
+
+		/* On recupère sa liste courante (non nulle) */
+		ListeDeCourse listeCourante = user.listeDeCourse.get(0);
+		listeCourante.addAliment(aliment);
+		
+		/* on trie à nouveau les listes pour ajouter l'aliment dans la bonne section */
+		List<Aliment> articles = listeCourante.article;
+		List<Aliment> artFruitsLegumes = new ArrayList<Aliment>();
 		List<Aliment> artViandes = new ArrayList<Aliment>();
 		List<Aliment> artLaitages = new ArrayList<Aliment>();
 		List<Aliment> artBoissons= new ArrayList<Aliment>() ;
 		List<Aliment> artAutre = new ArrayList<Aliment>();
 		List<Aliment> artEpicerie = new ArrayList<Aliment>();
+
+		/* tri par section */
+		ApplicationUtils.misAJourListes(articles, artFruitsLegumes, artViandes, artLaitages, artBoissons, artAutre, artEpicerie);
+
+		renderArgs.put("artFruitsLegumes", artFruitsLegumes);
+		renderArgs.put("artLaitages", artLaitages);
+		renderArgs.put("artViandes", artViandes);
+		renderArgs.put("artBoissons", artBoissons);
+		renderArgs.put("artAutre", artAutre);
+		renderArgs.put("artEpicerie", artEpicerie);
+		index();
+	}
+	
+	public static void supprimeAlimentListe(Long id) {
 		
-		Iterator iter = articles.iterator();	
-		while (iter.hasNext()) {
-			Aliment cour = (Aliment) iter.next();
-			switch (cour.section.name()) {
-			case "FruitsLegumes":
-				artFruitsLegumes.add(cour);
-				break;
-			case "Laitages":
-				artLaitages.add(cour);
-				break;	
-			case "Viandes":
-				artViandes.add(cour);
-				break;	
-			case "Boissons":
-				artBoissons.add(cour);
-				break;
-			case "Autre":
-				artAutre.add(cour);
-				break;	
-			case "Epicerie":
-				artEpicerie.add(cour);
-				break;
-			
-			default:
-				break;
-			}
-		}	      
-        renderArgs.put("artFruitsLegumes", artFruitsLegumes);
-        renderArgs.put("artLaitages", artLaitages);
-        renderArgs.put("artViandes", artViandes);
-        renderArgs.put("artBoissons", artBoissons);
-        renderArgs.put("artAutre", artAutre);
-        renderArgs.put("artEpicerie", artEpicerie);
-    	index();
-    }
-    
+		Aliment aliment = Aliment.findById(id);
+		aliment.delete();
+		index();
+	}
+
 }
