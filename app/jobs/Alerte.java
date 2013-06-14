f7ba9e0675bf243609253456e9d35a43137037b5
Emilie Paillous <emilie.paillous@gmail.com>
2013-06-14 02:20:02 +0200

    alertes ok

---
+++ app/jobs/Alerte.java
@@ -0,0 +1,50 @@
+package jobs;
+ 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
+
+import models.Aliment;
+import models.EtatFrigo;
+import models.User;
+import play.jobs.*;
+import play.libs.Mail;
+import notifiers.*;
+ 
+@Every("24h")
+@OnApplicationStart(async=true)
+public class Alerte extends Job {
+    
+    public void doJob() throws EmailException {
+    	System.out.println("je suis dans alerte!");
+		/* On verifie les dates de péremption de tous les aliments contenus dans le dernier etat Frigo pour tous les utilisateurs */
+    	List<User> users = User.findAll();
+    	Calendar date=new GregorianCalendar();
+    	/* On recupère la date du lendemain */
+    	date.add(Calendar.DATE, 1);
+    	Iterator iter = users.iterator();	
+    	while (iter.hasNext()) {
+    		User user = (User) iter.next();
+        	EtatFrigo dernierEtat = EtatFrigo.find("user like ? order by date desc", user).first();
+        	List<Aliment> aliments = dernierEtat.aliment;
+        	Iterator iter2 = aliments.iterator();
+        	while (iter2.hasNext()) {
+        		Aliment aliment = (Aliment) iter2.next();
+        		if ((date.getTime().getDay() == aliment.peremption.getDay()) 
+        				&& (date.getTime().getMonth() == aliment.peremption.getMonth()) 
+        				&& (date.getTime().getYear() == aliment.peremption.getYear())) {   
+        			/* le produit périme demain ! */
+        			Mails.alerte(user, aliment.nom);
+        		}
+        	}
+    	}
+
+    	
+	}
+    
+}
---
+++ app/notifiers/Mails.java
@@ -0,0 +1,28 @@
+package notifiers;
+ 
+import models.Aliment;
+import models.User;
+
+import org.apache.commons.mail.*; 
+import play.*;
+import play.mvc.*;
+import java.util.*;
+ 
+public class Mails extends Mailer {
+ 
+   public static void alerte(User user, String aliment) {
+      setSubject("Alerte sur le produit %s", aliment);
+      addRecipient(user.email);
+      setFrom("MonFrigo <emilie.paillous@gmail.com>");
+      send(user);
+   }
+ 
+   public static void lostPassword(User user) {
+      String newpassword = user.motDePasse;
+      setFrom("MonFrigo <emilie.paillous@gmail.com>");
+      setSubject("Voici votre mot de Passe");
+      addRecipient(user.email);
+      send(user, newpassword);
+   }
+ 
+}
