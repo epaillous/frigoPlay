cb976d648269bfa2dda858688d333d1456eeaff8
Emilie Paillous <emilie.paillous@gmail.com>
2013-06-15 15:00:43 +0200

    contenu du frigo ok

--- app/models/EtatFrigo.java
+++ app/models/EtatFrigo.java
@@ -44,5 +44,17 @@ public class EtatFrigo extends Model {
 //	    this.save();
 //	    return this;
 //	}
+	
+	public EtatFrigo addAliment(String aliment) {
+		AlimentConnu present = AlimentConnu.find("byNom", aliment).first();
+		if (present == null) {			
+	    Aliment newAliment = new Aliment(aliment, this, Section.Autre).save();
+	    this.aliment.add(newAliment);
+		} else {	
+			new Aliment(aliment, this, present.section).save();
+		}
+	    this.save();
+	    return this;
+	}
 
 }
