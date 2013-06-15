424f0ff2410babce5fcfaafc9f46923429ab81e2
Emilie Paillous <emilie.paillous@gmail.com>
2013-06-15 13:33:09 +0200

    ajout champ aliment connu

--- app/models/AlimentConnu.java
+++ app/models/AlimentConnu.java
@@ -1,5 +1,7 @@
 package models;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 
 import play.data.validation.Required;
@@ -11,6 +13,8 @@ public class AlimentConnu extends Model {
 	@Required
 	public String nom;
 	public Section section;
+	/* lien vers les images */
+	public List<String> images;
 	
 	public AlimentConnu(String nom, Section section) {
 		super();
