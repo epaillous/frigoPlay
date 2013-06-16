5f4275e3dd685159521ab38df519b7cb88c3c379
Maïlys Reneaume <mailys.reneaume@gmail.com>
2013-06-16 13:17:33 +0200

    chevron

---
+++ public/javascripts/chevron.js
@@ -0,0 +1,9 @@
+var srcChevron="/public/images/chevronBas.svg";
+
+function changeChevron() {		
+	if(srcChevron=="/public/images/chevronBas.svg"){
+		srcChevron="/public/images/chevronHaut.svg";
+	} else {
+		srcChevron="/public/images/chevronBas.svg";
+	}
+}
